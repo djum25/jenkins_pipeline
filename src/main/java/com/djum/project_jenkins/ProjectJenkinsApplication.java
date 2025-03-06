45d42550a8f852e80e5818b3ab61a241c3d9fa91
djum25 <djum25@gmail.com>
2025-03-06 15:04:06 +0000

    init

---
+++ src/main/java/com/djum/project_jenkins/ProjectJenkinsApplication.java
@@ -0,0 +1,22 @@
+package com.djum.project_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProjectJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJenkinsApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String entryPoint(){
+		return "Hello world";
+	}
+
+}
---
+++ src/test/java/com/djum/project_jenkins/ProjectJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djum.project_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
