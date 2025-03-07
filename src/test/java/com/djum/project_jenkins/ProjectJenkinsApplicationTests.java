7ff5185a62b149394a3eeb1f531b6519549923ba
djum25 <djum25@gmail.com>
2025-03-07 09:42:53 +0000

    First test

--- src/test/java/com/djum/project_jenkins/ProjectJenkinsApplicationTests.java
+++ src/test/java/com/djum/project_jenkins/ProjectJenkinsApplicationTests.java
@@ -2,12 +2,14 @@ package com.djum.project_jenkins;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class ProjectJenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.isTrue(true, "test reussi");
 	}
 
 }
